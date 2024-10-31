fea43bb18b4b1f0279b9afa78318190d197b878c
Mikee Ojastro <mikeeojastro@gmail.com>
2024-10-31 20:02:53 +0800

    October 31, 2024

--- src/App.js
+++ src/App.js
@@ -9,10 +9,17 @@ function App() {
   const [count, setCount] = useState(tasks.length);
 
   const handleTasks = (newTask) => {
+    // newTask = user input which we will get from the AddTask Component
+    // newCount = updating for id
+    let newCount = count + 1;
+    setCount(newCount);
+    // [...prevTasks, newTask] = spread operator - creating a new array that contains the previous tasks and the newly created task
+    setTasks((prevTasks) => [...prevTasks, newTask]);
   }
 
   const handleDeleteTask = (taskId) => {
-
+    // filter - matches the condition then it will return the element if the condition is true. This will create a new array
+    setTasks((prevTasks) => prevTasks.filter((task) => task.id !== taskId));
   }
 
   return (
@@ -22,11 +29,13 @@ function App() {
           return (
             <div key={task.id}> 
               <span>{task.task_name}</span>
+              <button onClick={() => handleDeleteTask(task.id)}>Delete</button>
             </div>
           );
         })
       }
 
+      <AddTask handleAddTasks={handleTasks} newId={count} />
     </div>
   );
 }
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,17 +1,29 @@
 import { useState } from "react";
 
 function AddTask(props){
+    const { handleAddTasks, newId } = props;
 
     // declaration for the new task 
     const [toDo, setToDo] = useState('');
 
     const addTaskHandler = (event) => {
+        event.preventDefault();
 
+        const newTaskObject = {
+            task_name: toDo,
+            id: newId
+        }
+
+        handleAddTasks(newTaskObject);
+        setToDo('');
     }
 
     return (
         <div className="addTask">
-
+            <form onSubmit={addTaskHandler}>
+                <input value={toDo} placeholder="Add task here" onChange={(event) => setToDo(event.target.value)}></input>
+                <button>Add Task</button>
+            </form>
         </div>
     );
 }
