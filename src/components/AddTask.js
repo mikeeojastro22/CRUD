68935068ab99bfbfad2e2b55d2ea11bee7b29f17
Mikee Ojastro <mikeeojastro@gmail.com>
2024-10-31 18:57:12 +0800

    October 31, 2024

---
+++ src/components/AddTask.js
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+function AddTask(props){
+
+    // declaration for the new task 
+    const [toDo, setToDo] = useState('');
+
+    const addTaskHandler = (event) => {
+
+    }
+
+    return (
+        <div className="addTask">
+
+        </div>
+    );
+}
+
+export default AddTask;
